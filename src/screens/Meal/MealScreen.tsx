b9afd785583aaacd2cc03e663ca04d892ba5cb7b
Will Lam <wunyinwilliam@gmail.com>
2024-06-11 15:37:30 +0800

    UPDATE: Quiz View

--- src/screens/Meal/MealScreen.tsx
+++ src/screens/Meal/MealScreen.tsx
@@ -1,22 +1,36 @@
-import React from 'react';
-import { View, Text, FlatList, Image, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+import {
+    FlatList,
+    Image,
+    SafeAreaView,
+    ScrollView,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+} from "react-native"
+import {
+    getRecommendMealImage,
+    getRecommendRestaurantImage,
+    getRecommendRestaurantLogoImage,
+} from "@/resources/images"
+
+import React from "react"
+import RecommendMeal from "@/models/RecommendMeal"
+import RecommendRestaurant from "@/models/RecommendRestaurant"
 import { SafeScreen } from "@/components/template"
-import sampleRecommendMeal from '@/sample-data/sample-recommendmeal';
-import sampleRestaurants from '@/sample-data/sample-recommendrestaurant';
-import useViewModel from './useViewModel';
-import RecommendMeal from '@/models/RecommendMeal';
-import { getRecommendMealImage, getRecommendRestaurantLogoImage, getRecommendRestaurantImage } from '@/resources/images';
-import RecommendRestaurant from '@/models/RecommendRestaurant';
+import sampleRecommendMeal from "@/sample-data/sample-recommendmeal"
+import sampleRestaurants from "@/sample-data/sample-recommendrestaurant"
+import useViewModel from "./useViewModel"
 
 const MealScreen = ({ navigation }: MainBottomTabScreenProps) => {
-
-    const {
-        styles
-    } = useViewModel();
+    const { styles } = useViewModel()
 
     const renderRecommendedMealItem = ({ item }: { item: RecommendMeal }) => (
         <View style={styles.card}>
-            <Image source={getRecommendMealImage(item.id)} style={styles.image} />
+            <Image
+                source={getRecommendMealImage(item.id)}
+                style={styles.image}
+            />
             <Text style={styles.name}>{item.name}</Text>
             <Text style={styles.calories}>{item.calories} Calories</Text>
             <Text style={styles.description}>"{item.description}"</Text>
@@ -24,61 +38,94 @@ const MealScreen = ({ navigation }: MainBottomTabScreenProps) => {
                 <Text style={styles.seeMore}>See More</Text>
             </TouchableOpacity>
         </View>
-    );
+    )
 
-    const renderRestaurantDicountItem = ({ item }: { item: RecommendRestaurant}) => (
-        <TouchableOpacity style={styles.restaurantDiscountCard} onPress={() => {}}>
-            <Image source={getRecommendRestaurantLogoImage(item.id)} style={styles.restaurantImage} />
+    const renderRestaurantDicountItem = ({
+        item,
+    }: {
+        item: RecommendRestaurant
+    }) => (
+        <TouchableOpacity
+            style={styles.restaurantDiscountCard}
+            onPress={() => {}}
+        >
+            <Image
+                source={getRecommendRestaurantLogoImage(item.id)}
+                style={styles.restaurantImage}
+            />
             <View style={styles.restaurantDiscountTextContainer}>
-                <Text style={styles.restaurantDiscountTitle}>{item.discount[0].discountPercentage}% OFF</Text>
-                <Text style={styles.restaurantDiscountTerm}>{item.discount[0].terms}</Text>
-                <Text style={styles.restanrantDiscountEndDate}>{item.discount[0].validUntil}</Text>
+                <Text style={styles.restaurantDiscountTitle}>
+                    {item.discount[0].discountPercentage}% OFF
+                </Text>
+                <Text style={styles.restaurantDiscountTerm}>
+                    {item.discount[0].terms}
+                </Text>
+                <Text style={styles.restanrantDiscountEndDate}>
+                    {item.discount[0].validUntil}
+                </Text>
             </View>
         </TouchableOpacity>
-    );
+    )
 
-    const RecommendedRestaurantItem = ({ item }: { item: RecommendRestaurant }) => (
+    const RecommendedRestaurantItem = ({
+        item,
+    }: {
+        item: RecommendRestaurant
+    }) => (
         <TouchableOpacity style={styles.restaurantCard} onPress={() => {}}>
             <View style={styles.restaurantTextContainer}>
                 <Text style={styles.restaurantTitle}>{item.name}</Text>
                 <Text style={styles.restaurantCategory}>{item.category}</Text>
-                <Text style={styles.restaurantDescription}>{item.description}</Text>
+                <Text style={styles.restaurantDescription}>
+                    {item.description}
+                </Text>
             </View>
-            <Image source={getRecommendRestaurantImage(item.id)} style={styles.restaurantImage} />
+            <Image
+                source={getRecommendRestaurantImage(item.id)}
+                style={styles.restaurantImage}
+            />
         </TouchableOpacity>
-    );
+    )
 
     return (
-        <ScrollView style={styles.container}>
-            <Text style={styles.header}>Let's Check Food Nutrition & Calories</Text>
-            <Text style={styles.subHeader}>Select food type to see calories</Text>
-            <FlatList
-                data={sampleRecommendMeal}
-                renderItem={renderRecommendedMealItem}
-                keyExtractor={(item) => item.id.toString()}
-                horizontal
-                showsHorizontalScrollIndicator={false}
-                contentContainerStyle={styles.list}
-            />
-            <Text style={styles.header}>Let's See Recommend Restaurants</Text>
-            <Text style={styles.subHeader}>Your health matters</Text>
-            <View style={styles.restaurantList}>
-                { sampleRestaurants.map((item) => (
-                    <RecommendedRestaurantItem key={item.id} item={item}/>
-                ))}
-            </View>
-            <Text style={styles.header}>Get Discount And Stay Healthy</Text>
-            <Text style={styles.subHeader}>See the promotion</Text>
-            <FlatList
-                data={sampleRestaurants}
-                renderItem={renderRestaurantDicountItem}
-                keyExtractor={(item) => item.id.toString()}
-                horizontal
-                showsHorizontalScrollIndicator={false}
-                contentContainerStyle={styles.list}
-            />
-        </ScrollView>
-    );
+        <SafeScreen>
+            <ScrollView style={styles.container}>
+                <Text style={styles.header}>
+                    Let's Check Food Nutrition & Calories
+                </Text>
+                <Text style={styles.subHeader}>
+                    Select food type to see calories
+                </Text>
+                <FlatList
+                    data={sampleRecommendMeal}
+                    renderItem={renderRecommendedMealItem}
+                    keyExtractor={(item) => item.id.toString()}
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    contentContainerStyle={styles.list}
+                />
+                <Text style={styles.header}>
+                    Let's See Recommend Restaurants
+                </Text>
+                <Text style={styles.subHeader}>Your health matters</Text>
+                <View style={styles.restaurantList}>
+                    {sampleRestaurants.map((item) => (
+                        <RecommendedRestaurantItem key={item.id} item={item} />
+                    ))}
+                </View>
+                <Text style={styles.header}>Get Discount And Stay Healthy</Text>
+                <Text style={styles.subHeader}>See the promotion</Text>
+                <FlatList
+                    data={sampleRestaurants}
+                    renderItem={renderRestaurantDicountItem}
+                    keyExtractor={(item) => item.id.toString()}
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    contentContainerStyle={styles.list}
+                />
+            </ScrollView>
+        </SafeScreen>
+    )
 }
 
 export default MealScreen
--- src/screens/Quiz/QuizScreen.tsx
+++ src/screens/Quiz/QuizScreen.tsx
@@ -1,104 +1,169 @@
-import React, { useEffect } from "react";
-import {
-  Text,
-  View,
-  TouchableOpacity,
-  Animated
-} from "react-native"
-import { SafeScreen } from "@/components/template";
-import { MainBottomTabScreenProps } from "@/navigators/navigation";
+import { Animated, Text, TouchableOpacity, View } from "react-native"
+import React, { useEffect } from "react"
+
+import { MainBottomTabScreenProps } from "@/navigators/navigation"
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons"
-import useViewModel from "./useViewModel";
+import { SafeScreen } from "@/components/template"
+import { TabRoute } from "@/navigators/ScreenRoute"
+import useViewModel from "./useViewModel"
 
 function QuizScreen({ navigation }: MainBottomTabScreenProps) {
-
-    const { 
-        questions, 
+    const {
+        questions,
         userAnswers,
         setUserAnswers,
-        fadeAnim, 
-        fadeIn, 
-        fadeOut, 
-        styles, 
-        currentQuestionIndex, 
+        fadeAnim,
+        fadeIn,
+        fadeOut,
+        styles,
+        currentQuestionIndex,
         setCurrentQuestionIndex,
-        stage, 
-        setStage 
-    } = useViewModel();
+        stage,
+        setStage,
+    } = useViewModel()
+
+    const handleGetHealthInformation = () => {
+        navigation.navigate(TabRoute.ProfileScreen)
+    }
 
     const handleStart = () => {
-        setStage('quiz');
-        fadeIn();
+        setStage("quiz")
+        fadeIn()
     }
 
     const handleAnswer = (option: string) => {
-        const newAnswers = [...userAnswers, { question: questions[currentQuestionIndex].questionText, answer: option }];
-        setUserAnswers(newAnswers);
+        const newAnswers = [
+            ...userAnswers,
+            {
+                question: questions[currentQuestionIndex].questionText,
+                answer: option,
+            },
+        ]
+        setUserAnswers(newAnswers)
 
         if (currentQuestionIndex < questions.length - 1) {
-            fadeOut();
+            fadeOut()
         } else {
-            fadeOut();
-            setStage('end');
+            fadeOut()
+            setStage("end")
         }
-    };
+    }
 
     const handleRestart = () => {
-        setStage('start');
-        setCurrentQuestionIndex(0);
-        fadeIn();
+        setStage("start")
+        setCurrentQuestionIndex(0)
+        fadeIn()
     }
 
     useEffect(() => {
-        if (stage === 'quiz') {
-            fadeIn();
+        if (stage === "quiz") {
+            fadeIn()
         }
-    }, [stage, currentQuestionIndex]);
+    }, [stage, currentQuestionIndex])
 
     return (
         <SafeScreen>
             <View style={styles.container}>
-                {stage === 'start' && (
+                {stage === "start" && (
                     <View style={styles.startContainer}>
-                        <Text style={styles.title}>Welcome to Elderly Nutrition Assessment Tool!</Text>
-                        <Text style={styles.description}>
-                            Welcome to the Elderly Nutrition Assessment Tool. This questionnaire is designed to help us understand the dietary needs and health challenges faced by older adults. Your responses will enable us to provide tailored nutritional advice that can contribute to improved health and well-being. The questionnaire consists of multiple-choice questions and should take about 5-10 minutes to complete. Please answer each question based on the current conditions and daily habits of the elderly individual in question. Thank you for participating and helping us better serve the nutritional needs of the elderly community. Tap 'Start' to begin!
+                        <Text style={styles.title}>
+                            Welcome to Elderly Nutrition Assessment Tool!
                         </Text>
-                        <TouchableOpacity style={styles.startButton} onPress={handleStart}>
+                        <Text style={styles.description}>
+                            Welcome to the Elderly Nutrition Assessment Tool.
+                            This questionnaire is designed to help us understand
+                            the dietary needs and health challenges faced by
+                            older adults. Your responses will enable us to
+                            provide tailored nutritional advice that can
+                            contribute to improved health and well-being. The
+                            questionnaire consists of multiple-choice questions
+                            and should take about 5-10 minutes to complete.
+                            Please answer each question based on the current
+                            conditions and daily habits of the elderly
+                            individual in question. Thank you for participating
+                            and helping us better serve the nutritional needs of
+                            the elderly community. Tap 'Start' to begin!
+                        </Text>
+                        <TouchableOpacity
+                            style={styles.startButton}
+                            onPress={handleStart}
+                        >
                             <Text style={styles.buttonText}>Start</Text>
                         </TouchableOpacity>
                     </View>
                 )}
 
-                {stage === 'quiz' && (
+                {stage === "quiz" && (
                     <View style={styles.quizContainer}>
-                        <TouchableOpacity style={styles.restartButton} onPress={handleRestart}>
-                            <MaterialCommunityIcons name="restart" size={32} color="#000"/>
+                        <TouchableOpacity
+                            style={styles.restartButton}
+                            onPress={handleRestart}
+                        >
+                            <MaterialCommunityIcons
+                                name="restart"
+                                size={32}
+                                color="#000"
+                            />
                         </TouchableOpacity>
-                        <Animated.View style={[styles.fadeContainer, {opacity: fadeAnim}]}>
+                        <Animated.View
+                            style={[
+                                styles.fadeContainer,
+                                { opacity: fadeAnim },
+                            ]}
+                        >
                             <Text style={styles.question}>
                                 {questions[currentQuestionIndex].questionText}
                             </Text>
-                            {questions[currentQuestionIndex].options.map((option, oIndex) => (
-                                <TouchableOpacity key={oIndex} style={styles.optionButton} onPress={() => handleAnswer(option)}>
-                                    <Text style={styles.optionText}>{option}</Text>
-                                </TouchableOpacity>
-                            ))}
+                            {questions[currentQuestionIndex].options.map(
+                                (option, oIndex) => (
+                                    <TouchableOpacity
+                                        key={oIndex}
+                                        style={styles.optionButton}
+                                        onPress={() => handleAnswer(option)}
+                                    >
+                                        <Text style={styles.optionText}>
+                                            {option}
+                                        </Text>
+                                    </TouchableOpacity>
+                                )
+                            )}
                         </Animated.View>
                     </View>
                 )}
 
-                {stage === 'end' && (
+                {stage === "end" && (
                     <View style={styles.endContainer}>
-                        <Text style={styles.finishText}>You have finished the quiz!</Text>
-                        <Text style={styles.resultsTitle}>Your Answers:</Text>
-                        {userAnswers.map((item, index) => (
-                            <Text key={index} style={styles.answerText}>
-                                Q: {item.question} - A: {item.answer}
+                        <View style={styles.stretchContainer}>
+                            <Text style={styles.finishText}>
+                                You have finished the quiz!
                             </Text>
-                        ))}
-                        <TouchableOpacity style={styles.homeButton} onPress={handleRestart}>
+                            <Text style={styles.resultsTitle}>
+                                Your Answers:
+                            </Text>
+                            {userAnswers.map((item, index) => (
+                                <View key={index} style={styles.resultView}>
+                                    <Text style={styles.questionText}>
+                                        Q: {item.question}
+                                    </Text>
+                                    <Text style={styles.answerText}>
+                                        A: {item.answer}
+                                    </Text>
+                                </View>
+                            ))}
+                        </View>
+                        <TouchableOpacity
+                            style={styles.homeButton}
+                            onPress={handleRestart}
+                        >
                             <Text style={styles.buttonText}>Restart</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            style={styles.getHealthInformationButton}
+                            onPress={handleGetHealthInformation}
+                        >
+                            <Text style={styles.buttonText}>
+                                View Health Information
+                            </Text>
                         </TouchableOpacity>
                     </View>
                 )}
@@ -107,4 +172,4 @@ function QuizScreen({ navigation }: MainBottomTabScreenProps) {
     )
 }
 
-export default QuizScreen;
+export default QuizScreen
